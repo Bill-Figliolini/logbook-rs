508f463f60310165217294042b3744041bd4eed4
Bill Figliolini <BillFigliolini@gmail.com>
2025-12-09 15:39:00 -0500

    Added basic writing functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Result, bail};
+
+use std::{fs::File, io::Write};
+
+fn main() -> Result<()> {
+    let args: Vec<_> = std::env::args().skip(1).collect();
+
+    if args.is_empty() {
+        bail!("Usage: logbook <Message>")
+    }
+    let path = "logbook.txt";
+    let mut logbook = File::options().create(true).append(true).open(path)?;
+    writeln!(logbook, "{}", args.join(" "))?;
+    Ok(())
 }
